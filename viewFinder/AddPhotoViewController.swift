25e37d16e386047f4777e010d911e303c2e5ffa5
Taryn <taryn@Apples-MacBook-Pro.local>
2019-08-13 13:28:48 -0400

    Iteration 1

---
+++ viewFinder/AddPhotoViewController.swift
@@ -0,0 +1,52 @@
+//
+//  AddPhotoViewController.swift
+//  viewFinder
+//
+//  Created by Taryn on 8/13/19.
+//  Copyright © 2019 Taryn. All rights reserved.
+//
+
+import UIKit
+
+class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imagePicker.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func takeSelfieTapped(_ sender: UIButton) {
+        
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    
+    @IBAction func photoLibraryTapped(_ sender: UIButton) {
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    
+        
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
